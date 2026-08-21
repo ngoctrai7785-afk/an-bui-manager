2d41e4a525dd8e9440659547bfa2cc0dc454a980
ngoctrai7785-afk <ngoctrai7785@gmail.com>
2026-08-21 13:36:21 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ab123-an-bui-v7';
+const CACHE='ab123-an-bui-v9';
 const SHELL=['./','./index.html','./AB123_MANAGER.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./favicon-64.png'];
 
 self.addEventListener('install', event => {
